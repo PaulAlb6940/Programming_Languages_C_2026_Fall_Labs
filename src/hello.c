41c4d473e81c23b92befba34fc5b39364c77b911
Paul Alberti <paul.alberti4083@gmail.com>
2026-09-16 12:38:33 +0000

    update hello message

--- src/hello.c
+++ src/hello.c
@@ -3,8 +3,9 @@
 
 int main(int argc, char *argv[]) {
   // This is your first C program my friend
-  printf("Hello, RTU World from C Lab in 2025!\n");
+  printf("Hello, from Alberti Paul! \n");
   printf("You passed %d argument(s).\n", argc - 1);
+  
   for (int i = 1; i < argc; ++i) {
     printf("  arg[%d] = %s\n", i, argv[i]);
   }
